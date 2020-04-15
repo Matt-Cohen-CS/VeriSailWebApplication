c0a33c5cd556b1cb55e28280fe6d94d1127e4a9d
Matthew <matt.cohen.cs@gmail.com>
2020-04-14 23:17:27 -0400

    Updated APIService

--- Angular-WebApp2/src/app/Services/api-service.service.ts
+++ Angular-WebApp2/src/app/Services/api-service.service.ts
@@ -4,43 +4,38 @@ import { Observable } from 'rxjs'; // Added
 import { Test } from './sample';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class ApiServiceService {
-  readonly ROOT_URL = 'http://ec2-3-89-226-21.compute-1.amazonaws.com:3000'; //Added
-  //readonly ROOT_URL = 'http://localhost:8080';
-  posts: Observable<any>; //Added
-  constructor(private http:HttpClient) { }
-/*
+	readonly ROOT_URL = 'http://ec2-3-89-226-21.compute-1.amazonaws.com:3000'; //Added
+	//readonly ROOT_URL = 'http://localhost:8080';
+	posts: Observable<any>; //Added
+	constructor(private http: HttpClient) {}
+	/*
 Added
-*/ 
-  getPost(): Observable<any>{
-   // this.posts = this.http.get(this.ROOT_URL) 
-   return this.http.get<any>(this.ROOT_URL+"/manufacturers");
-  }
-  updateManu(id,data): Observable<any>{
-    // this.posts = this.http.get(this.ROOT_URL) 
-   return this.http.put<any>("https://cors-anywhere.herokuapp.com/"+this.ROOT_URL+"/manufacturerupdate/"+id,data);
-   //return this.http.put<any>(this.ROOT_URL+"/manufacturerupdate/"+id,data,{
-    // headers: new HttpHeaders({
-    //   'Content-type':'application/json',
-    //   'Access-Control-Allow-Origin': '*',
-    //   'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,DELETE,PUT'
-  //   })
-  //  });
-  }
-  getPostByID(id:string): Observable<any>{
-    // this.posts = this.http.get(this.ROOT_URL) 
-    return this.http.get<any>(this.ROOT_URL+"/manufacturers/"+id);
-   }
-  errorHandler(error: HttpErrorResponse){
-    return Observable.throw(error.message || "Server Error");
-  }
-  addManu(data){
-    return this.http.post<any>(this.ROOT_URL+"/newmanufacturer",data,{
-      headers: new HttpHeaders({
-        'Content-type':'application/json'
-      })
-    });
-  }
+*/
+
+	getPost(): Observable<any> {
+		// this.posts = this.http.get(this.ROOT_URL)
+		return this.http.get<any>(this.ROOT_URL + '/manufacturers');
+	}
+	updateManu(id, data): Observable<any> {
+		// this.posts = this.http.get(this.ROOT_URL)
+		//return this.http.put<any>("https://cors-anywhere.herokuapp.com/"+this.ROOT_URL+"/manufacturerupdate/"+id,data);
+		return this.http.put<any>(this.ROOT_URL + '/manufacturerupdate/' + id, data);
+	}
+	getPostByID(id: string): Observable<any> {
+		// this.posts = this.http.get(this.ROOT_URL)
+		return this.http.get<any>(this.ROOT_URL + '/manufacturers/' + id);
+	}
+	errorHandler(error: HttpErrorResponse) {
+		return Observable.throw(error.message || 'Server Error');
+	}
+	addManu(data) {
+		return this.http.post<any>(this.ROOT_URL + '/newmanufacturer', data, {
+			headers: new HttpHeaders({
+				'Content-type': 'application/json'
+			})
+		});
+	}
 } // End of class
