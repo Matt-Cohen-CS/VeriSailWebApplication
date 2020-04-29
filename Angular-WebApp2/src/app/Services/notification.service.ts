eaa5a9e4aec223ee813a1ca76d36d60b6429d41e
Matthew <matt.cohen.cs@gmail.com>
2020-04-28 20:56:41 -0400

    Added an overflow global style to all mat-tables, fixed paginator in event

--- Angular-WebApp2/src/app/Services/notification.service.ts
+++ Angular-WebApp2/src/app/Services/notification.service.ts
@@ -8,17 +8,17 @@ export class NotificationService {
 	constructor(public snackBar: MatSnackBar) {}
 
 	config: MatSnackBarConfig = {
-		duration: 3000,
+		duration: 4000,
 		horizontalPosition: 'right',
 		verticalPosition: 'top'
 	};
 
 	success(msg) {
 		this.config['panelClass'] = [ 'notification', 'success' ];
-		this.snackBar.open(msg, '', this.config);
+		this.snackBar.open(msg, 'Close', this.config);
 	}
 	warn(msg) {
 		this.config['panelClass'] = [ 'notification', 'warn' ];
-		this.snackBar.open(msg, '', this.config);
+		this.snackBar.open(msg, 'Close', this.config);
 	}
 }
